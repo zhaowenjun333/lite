8a343746510d295b4a8ca7e0dc182fe5c8889f02
hua03 <295506287@qq.com>
2017-07-15 11:53:42 +0800

    update

---
+++ Code-Snippet/shell/autopush.sh
@@ -0,0 +1,9 @@
+# @description: git push脚本 自动提交
+# @Author: hua03
+# @Date:   2017-07-03 15:43:07
+# @Last Modified by:   hua03
+# @Last Modified time: 2017-07-03 15:46:22
+
+git add .
+git commit -m 'update'
+git push
